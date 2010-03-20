72d2a1c77bbc9fafb25d3de8ee9a39e1d64d7efc
Chase Sterling <chase.sterling@gmail.com>
2010-03-20 19:41:31 -0400

    removed personal references in create_dev_link.py

--- create_dev_link.sh
+++ create_dev_link.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-cd /home/chase/deluge/deluge/plugins/autoadd
 mkdir temp
 export PYTHONPATH=./temp
 python setup.py build develop --install-dir ./temp
-cp ./temp/AutoAdd.egg-link /home/chase/.config/deluge/plugins
+cp ./temp/AutoAdd.egg-link ~/.config/deluge/plugins
 rm -fr ./temp
